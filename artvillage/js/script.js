ef09ce7d218912b069c570b0feaa001c9f882147
seoeunus89 <seoeunus89@naver.com>
2020-05-29 17:52:50 +0900

    20200529

--- artvillage/js/script.js
+++ artvillage/js/script.js
@@ -1,3 +1,13 @@
+$(function(){
+    $(".menu").mouseover(function(){
+        $(".subNav").stop().slideDown();
+        $(".subNav").css("display","flex");
+    });
+    $(".menu").mouseleave(function(){
+        $(this).children(".subNav").stop().slideUp();
+    });
+});
+
 setInterval(function(){
     // setInterval(); 함수 반복 실행
     $(".slide_box").delay("2500");
@@ -7,6 +17,3 @@ setInterval(function(){
     $(".slide_box").delay("2500");
     $(".slide_box").animate({marginTop: "0"}, "500");
 });
-
-
-
